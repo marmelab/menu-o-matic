2117e1a7403061d6fd3c02cff279ee6f076ed1b4
Francois Zaninotto <fzaninotto@gmail.com>
2013-08-13 18:39:39 +0200

    Add backbone version of menu list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,128 @@
+$(function(){
+
+  var Menu = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        title: '',
+        url: '',
+        order: Menus.nextOrder()
+      };
+    }
+  });
+
+  var MenuList = Backbone.Collection.extend({
+    model: Menu,
+    localStorage: new Backbone.LocalStorage("menus-backbone"),
+    nextOrder: function() {
+      if (!this.length) return 1;
+      return this.last().get('order') + 1;
+    },
+    comparator: 'order'
+  });
+
+  var Menus = new MenuList();
+  var menuFormView;
+
+  var MenuView = Backbone.View.extend({
+    tagName:  "li",
+    events: {
+      "click"     : "select"
+    },
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+    render: function() {
+      this.$el
+        .addClass('item')
+        .attr({
+          'tabindex': this.model.get('order'),
+          'data-url': this.model.get('url'),
+          'title': this.model.get('url')
+        })
+        .text(this.model.get('title'));
+      return this;
+    },
+    select: function() {
+      if (menuFormView) {
+        menuFormView.remove();
+      }
+      menuFormView = new MenuFormView({ model: this.model });
+      $(".menuform")
+        .html(menuFormView.render().el)
+        .show();
+    }
+  });
+
+  var MenuFormView = Backbone.View.extend({
+    template: _.template($('#form-template').html()),
+    events: {
+      'change input[name="title"]': 'updateTitle',
+      'keyup input[name="title"]': 'updateTitle',
+      'change input[name="url"]': 'updateUrl',
+      'keyup input[name="url"]': 'updateUrl',
+      'click #delete': 'removeMenu',
+      'focus input': 'enterInput',
+      'blur input': 'leaveInput'
+    },
+    initialize: function() {
+      this.listenTo(this.model, 'destroy', this.remove);
+    },
+    render: function() {
+      this.$el.html(this.template(this.model.attributes));
+      return this;
+    },
+    updateTitle: function(e) {
+      this.model.set('title', this.$('input[name="title"]').val());
+    },
+    updateUrl: function(e) {
+      this.model.set('url', this.$('input[name="url"]').val());
+    },
+    removeMenu: function() {
+      this.model.destroy();
+    },
+    enterInput: function() {
+      if (this.onFormBlur) {
+        clearTimeout(this.onFormBlur);
+      }
+    },
+    leaveInput: function() {
+      this.model.save();
+      var self = this;
+      this.onFormBlur = setTimeout(function() {
+        self.remove();
+      }, 500);
+    }
+  });
+
+  var MenuBarView = Backbone.View.extend({
+    el: $(".menubar"),
+    events: {
+      'click #add': 'createMenu'
+    },
+    initialize: function() {
+      this.listenTo(this.collection, 'add', this.addOne);
+      this.listenTo(this.collection, 'reset', this.addAll);
+      this.listenTo(this.collection, 'all', this.render);
+      // fetch menus from storage
+      this.collection.fetch();
+    },
+    addOne: function(menu) {
+      var view = new MenuView({ model: menu });
+      this.$('ul').append(view.render().el);
+    },
+    addAll: function() {
+      this.collection.each(this.addOne, this);
+    },
+    createMenu: function() {
+      this.collection.create({title: "Nouveau menu"});
+    }
+  });
+
+  var App = new MenuBarView({ collection: Menus });
+
+});
+
 /**
  * sort items with an arbitrary order
  * @param {Array} keys - ordered keys
@@ -42,46 +167,7 @@ docReady(function() {
   pckry.getItemElements().forEach(function(elem) {
     pckry.bindDraggabillyEvents(new Draggabilly(elem));
   });
-
-  // make items selectable
-  $('.item').on('mouseup', function(e) {
-    var $el = $(this);
-    $('#item_id').val($el.attr('tabindex'));
-    $('#item_title').val($el.text());
-    $('#item_url').val($el.data('url'));
-    $('form').show();
-  });
-
-  // delete item when delete button is pressed
-  $('#delete').on('click', function(e) {
-    var target = $('.item[tabindex="' + $('#item_id').val() + '"]');
-    pckry.remove(target.get());
-    pckry.layout();
-    $('form').trigger('blur');
-  });
-
-  // update item based on form change
-  $('#item_title').on('change', function(e) {
-    $('.item[tabindex="' + $('#item_id').val() + '"]').text($(this).val());
-    pckry.layout();
-  });
-  $('#item_url').on('change', function(e) {
-    $('.item[tabindex="' + $('#item_id').val() + '"]').data('url', $(this).val());
-  });
-
-  // hide form on blur
-  var onFormBlur;
-  $('form input').blur(function() {
-    onFormBlur = setTimeout(function() {
-    $('form').trigger('blur');
-   }, 1000);
-  }).focus(function() {
-   clearTimeout(onFormBlur);
-  });
-  $('form').on('blur', function(e) {
-    $(this).hide();
-  }).hide();
-
+  
   function applySortOrder(sortOrder) {
     pckry.sortItems(sortOrder, function(item) {
       return item.element.getAttribute('tabindex');
