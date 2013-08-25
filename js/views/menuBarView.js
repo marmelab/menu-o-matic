f12b500a1db5c5b1508c55da5f67355ca69380b4
DjebbZ <khalid.jebbari@gmail.com>
2013-08-25 16:37:29 +0200

    Same as previous. I meant 'view-dot-dollar-el'

--- js/views/menuBarView.js
+++ js/views/menuBarView.js
@@ -42,8 +42,9 @@ define(function(require) {
     addOne: function(menu) {
       var view = new MenuView({ model: menu });
       this._views.push(view);
-      var elem = view.render().el;
-      $(elem).addClass('selected');
+      var elem = view.render().el,
+          $elem = view.$el;
+      $elem.addClass('selected');
       this.$('ul').append(elem);
       this.pckry.prepended(elem);
       this.updateLayout();
